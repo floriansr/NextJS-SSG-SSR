105dbdc24030e39440ebb5338b6940e000021073
floriansr <sueurfloriansf@gmail.com>
2020-10-20 18:12:17 +0200

    [add] fetch data before rendering

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+
+  // Instead of the file system,
+  // fetch post data from an external API endpoint
+  // const res = await fetch("..");
+  // return res.json();
+
+  // You can also query the database directly:
+
+  // import someDatabaseSDK from 'someDatabaseSDK'
+  // const databaseClient = someDatabaseSDK.createClient(...)
+
+  // export async function getSortedPostsData() {
+  //   // Instead of the file system,
+  //   // fetch post data from a database
+  //   return databaseClient.query('SELECT posts...')
+  // }
+}
 
 
-export default function Home() {
+
+export default function Home({ allPostsData }) {
+  console.log("getStaticProps -> allPostsData", allPostsData);
+
   return (
     <Layout home>
       <Head>
@@ -18,6 +47,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
