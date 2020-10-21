cdccd14dab2a13866919f1eb2cc161952faac1d4
floriansr <sueurfloriansf@gmail.com>
2020-10-21 14:07:16 +0200

    [fix] ES6 functions corrections

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,7 +53,7 @@ export default function Home({
   );
 }
 
-export const getStaticProps : GetStaticProps = async () {
+export const getStaticProps : GetStaticProps = async () => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -31,7 +31,7 @@ export default function Post({
   );
 }
 
-export const getStaticPaths : GetStaticPaths = async () {
+export const getStaticPaths : GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
