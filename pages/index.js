d857255f171f81f0f288182a9c06970e24ed5731
floriansr <sueurfloriansf@gmail.com>
2020-10-21 13:11:34 +0200

    [add] links and date formatted to pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import Head from 'next/head'
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+
+import Head from 'next/head'
+import Link from "next/link";
+import Date from "../components/date";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -42,7 +45,6 @@ export async function getStaticProps() {
 
 
 export default function Home({ allPostsData }) {
-  console.log("getStaticProps -> allPostsData", allPostsData);
 
   return (
     <Layout home>
@@ -64,11 +66,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
